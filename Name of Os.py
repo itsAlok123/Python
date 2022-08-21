2d2d58dbb8abd1d90f44fa378b06a6c3af9acd5f
itsAlok123 <111678044+itsAlok123@users.noreply.github.com>
2022-08-21 22:06:09 +0530

    Create Name of Os.py

---
+++ Name of Os.py
@@ -0,0 +1,10 @@
+# Python
+list=[3.14,2.54,"CPU","WINDOWS 10"]
+ 
+print(list)
+Output:
+[3.14, 2.54, 'CPU', 'WINDOWS 10']list=[3.14,2.54,"CPU","WINDOWS 10"]
+ 
+print(list)
+Output:
+[3.14, 2.54, 'CPU', 'WINDOWS 10']
