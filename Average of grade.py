ce7074a3a0ae1be558141cd6719c4a2bdcef9afd
itsAlok123 <111678044+itsAlok123@users.noreply.github.com>
2022-08-21 20:12:39 +0530

    Create Average of grade.py

---
+++ Average of grade.py
@@ -0,0 +1,6 @@
+# Python
+firstexam = input('your first exam : ')
+secondexam = input('your second exam : ')
+thirdexam = input('your third exam : ')
+average =(float(firstexam)+float(secondexam)+float(thirdexam))/3
+print("average :{0} ".format(average))
