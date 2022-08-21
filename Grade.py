fb594f4c48570a40663b15ce899f861cdf771675
itsAlok123 <111678044+itsAlok123@users.noreply.github.com>
2022-08-21 20:11:27 +0530

    Create Grade.py

---
+++ Grade.py
@@ -0,0 +1,5 @@
+# Python
+visagrade = input('enter your visa grade : ')
+finalgrade = input('enter your final grade : ')
+average =(float(visagrade)*0.3)+(float(finalgrade)*0.7)
+print("average :{0} ".format(average))
