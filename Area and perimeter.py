70343bc02aeb66829ccc7c7be9d9034157f47ab4
itsAlok123 <111678044+itsAlok123@users.noreply.github.com>
2022-08-21 21:44:06 +0530

    Create Area and perimeter.py

---
+++ Area and perimeter.py
@@ -0,0 +1,7 @@
+# Python
+short = input('Enter short side : ')
+tall = input('Enter tall side : ')
+area = int(short)*int(tall)
+perimeter =2*(int(short)+int(tall))
+print("area: {0}".format(alan))
+print("perimeter: {0}".format(cevre))
