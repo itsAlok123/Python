137da62ab74ab885a46e70882243e4d33175b397
itsAlok123 <111678044+itsAlok123@users.noreply.github.com>
2022-08-21 21:46:15 +0530

    Create Sum of numbers.py

---
+++ Sum of numbers.py
@@ -0,0 +1,7 @@
+# Python
+sumofnumbers=0;
+num1 = input('first number: ')
+num2 = input('second number: ')
+for i in range(int(sayi1)+1,int(sayi2)):
+sumofnumbers+=i
+print("Sum of numbers between {0} and {1} : {2}".format(num1,num2,sumofnumbers))
