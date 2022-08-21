9d608e1a16019c35e99f936be38ba728003e7ed8
itsAlok123 <111678044+itsAlok123@users.noreply.github.com>
2022-08-21 21:54:24 +0530

    Create Area of rectangle.py

---
+++ Area of rectangle.py
@@ -0,0 +1,10 @@
+# Python
+def areaRectangle(a, b): 
+    return (a * b) 
+  
+def perimeterRectangle(a, b): 
+    return (2 * (a + b)) 
+a = 5; 
+b = 6; print ("Area = ", areaRectangle(a, b))
+
+print ("Perimeter = ", perimeterRectangle(a, b))
