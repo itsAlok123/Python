c3faba8275f8ed17575ff646fbc487f6e75c44d5
itsAlok123 <111678044+itsAlok123@users.noreply.github.com>
2022-08-21 21:52:07 +0530

    Create Salary and raise rate.py

---
+++ Salary and raise rate.py
@@ -0,0 +1,6 @@
+# Python
+newsalary = 0
+salary = input("enter new salary : ")
+raise = input("salary raise rate(%) : ")
+newsalary = int(salary)+(int(salary)*int(raise)/100)
+print("increased salary :",newsalary)
